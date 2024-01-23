ac270a99fe14141a2f61bfb731056c912d4c4cda
hammadmirzaa <hamadbaig25@gmail.com>
2024-01-23 22:29:50 +0500

    completed menu items now working on categories

--- reactpr/src/05-menu/setup/src/App5.js
+++ reactpr/src/05-menu/setup/src/App5.js
@@ -2,9 +2,21 @@ import React, { useState } from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
+import './index.css'
 
 function App5() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories ] = useState([])
+  return <main>
+  <section className='menu-section' >
+  <div className="title">
+  <h2>our menu</h2>
+  <div className="underline"></div>
+  </div>
+  <Categories/>
+  <Menu items={menuItems} />
+  </section>
+  </main> ;
 }
 
 export default App5;
