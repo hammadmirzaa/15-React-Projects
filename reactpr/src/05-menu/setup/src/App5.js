5e5662805fd8ed4e589d1fcb64d79685215104d9
hammadmirzaa <hamadbaig25@gmail.com>
2024-01-23 22:56:05 +0500

    completed the whole project but it's without pictures

--- reactpr/src/05-menu/setup/src/App5.js
+++ reactpr/src/05-menu/setup/src/App5.js
@@ -4,16 +4,26 @@ import Categories from './Categories';
 import items from './data';
 import './index.css'
 
+const allCategories = ['all' , ...new Set(items.map((item)=> item.category )) ];
+
 function App5() {
   const [menuItems, setMenuItems] = useState(items)
-  const [categories, setCategories ] = useState([])
+  const [categories, setCategories ] = useState(allCategories);
+  const filterItems =((category)=>{
+    if(category==='all'){
+      setMenuItems(items);
+      return;
+    }
+const newItems = items.filter((item)=> item.category===category );
+setMenuItems(newItems);
+  })
   return <main>
   <section className='menu-section' >
   <div className="title">
   <h2>our menu</h2>
   <div className="underline"></div>
   </div>
-  <Categories/>
+  <Categories filterItems={filterItems} categories={categories}  />
   <Menu items={menuItems} />
   </section>
   </main> ;
