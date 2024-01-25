c3cc8818723ef7be5307baa788147cfb95c3af9e
hammadmirzaa <hamadbaig25@gmail.com>
2024-01-25 23:07:51 +0500

    copied the 6th project

--- reactpr/src/App.js
+++ reactpr/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import App5 from './05-menu/setup/src/App5';
+import App6 from './06-tabs/setup/src/App';
 
 function App() {
-  return <App5/>;
+  return <App6/>;
 }
 
 export default App;
